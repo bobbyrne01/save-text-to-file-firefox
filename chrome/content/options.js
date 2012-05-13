c38580486aeb4ee177c88a8a5a418345157d9ffa
Robert Byrne <bobbyrne01@gmail.com>
2012-05-13 02:08:34 +0100

    Initial commit of source, version 1.0.4

---
+++ chrome/content/options.js
@@ -0,0 +1,19 @@
+//	Author: Robert Byrne (bobbyrne01@gmail.com)
+//	Copyright 2012
+
+var HighlightedTextToFile = {
+    selectDir: function() {
+    
+    	const nsIFilePicker = Components.interfaces.nsIFilePicker;
+    	var fp = Components.classes["@mozilla.org/filepicker;1"]
+        						.createInstance(Components.interfaces.nsIFilePicker);
+    	fp.init(window, "Select directory to store text files", nsIFilePicker.modeGetFolder);
+    	
+    	var ret = fp.show();
+    	if (ret == nsIFilePicker.returnOK || ret == nsIFilePicker.returnReplace) {
+    		var prefManager = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
+			var userPrefPathToFile = prefManager.setCharPref("extensions.highlightedtexttofile.pathToFile", fp.file.path);
+    		document.getElementById('pathToFile').value = fp.file.path;
+    	}
+   	},
+};
---
+++ defaults/preferences/pref.js
@@ -0,0 +1,6 @@
+//	Author: Robert Byrne (bobbyrne01@gmail.com)
+//	Copyright 2012
+
+pref("extensions.highlightedtexttofile.fileName", 'HighlightedText');
+pref("extensions.highlightedtexttofile.timestamp", true);
+pref("extensions.highlightedtexttofile.pathToFile", '');
