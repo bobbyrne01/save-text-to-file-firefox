b7ed534d21b34b90b13d2c657195c9acf5d5b107
bobbyrne01 <bobbyrne01@gmail.com>
2013-10-15 01:16:54 +0100

    #18 User can select a local directory to save files, uses nsIFilePicker API

--- data/confirmDialog.js
+++ data/confirmDialog.js
@@ -1,7 +1,14 @@
-function save(){
-	addon.port.emit("dialogClosed", 'save');
-}
-
-function cancel(){
-	addon.port.emit("dialogClosed", 'cancel');
-}
+var SaveTextToFile_Panel = {
+		
+		selectDir: function() {
+			addon.port.emit("selectDir", '');
+		},
+    
+		save: function() {
+			addon.port.emit("dialogClosed", 'save');
+		},
+		
+		cancel: function() {
+			addon.port.emit("dialogClosed", 'cancel');
+		}
+};
