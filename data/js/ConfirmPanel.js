807ec54dc3b6d42ab8a563360f28c8a1b00cc465
Robert Byrne <bobbyrne01@gmail.com>
2013-11-25 12:11:47 +0000

    #18 Preferences are now updated every time user confirms settings on panel

--- data/js/ConfirmPanel.js
+++ data/js/ConfirmPanel.js
@@ -1,23 +1,18 @@
 window.addEventListener('click', function(event) {
 	
-	console.log('clicked');
-	
 	if (event.target.id.indexOf('pathToFileButton') == 0){
-		console.log('selectDir');
 		
 		SaveTextToFile_Panel.selectDir();
 		
 	}else if (event.target.id.indexOf('saveButton') == 0){
-		console.log('save');
 		
 		SaveTextToFile_Panel.save();
 		
 	}else if (event.target.id.indexOf('cancelButton') == 0){
-		console.log('cancel');
 		
-		SaveTextToFile_Panel.cancel();
-		
+		SaveTextToFile_Panel.cancel();	
 	}
+	
 }, false);
 
 
@@ -33,22 +28,30 @@ var SaveTextToFile_Panel = {
 			// send path to file and file name back to addon code
 			var selectedPrefs = '{'
 				+'"fileName":"' + document.getElementById("fileName").value + '", '
-			    +'"pathToFile":"' + document.getElementById("pathToFile").value + '"'
+			    +'"pathToFile":"' + document.getElementById("pathToFile").value + '", '
+			    +'"datestamp":"' + document.getElementById("datestamp").checked + '", '
+			    +'"timestamp":"' + document.getElementById("timestamp").checked + '", '
+			    +'"datestampInLine":"' + document.getElementById("datestampInLine").checked + '", '
+			    +'"timestampInLine":"' + document.getElementById("timestampInLine").checked + '", '
+			    +'"lineSeparator":"' + document.getElementById("lineSeparator").checked + '", '
+			    +'"currentURL":"' + document.getElementById("currentURL").checked + '", '
+			    +'"saveMode":"' + document.getElementById("saveMode").value + '", '
+			    +'"confirmPanel":"' + document.getElementById("confirmPanel").checked + '"'
 			    +'}';
 
 			self.port.emit("save", selectedPrefs);
 		},
 		
 		cancel: function() {
-			console.log('cancel in contentscript call function');
 			self.port.emit("cancel", '');
 		}
 };
 
 // listen for preferences message from addon code and set values of Panel UI
 self.port.on("prefs", function (prefs) {
-	console.log("got prefs");
 	var parsedPrefs = JSON.parse(prefs);
+	
+	console.log(parsedPrefs.fileName);
 	
 	document.getElementById("fileName").value = parsedPrefs.fileName;
 	document.getElementById("pathToFile").value = parsedPrefs.pathToFile;
@@ -59,4 +62,5 @@ self.port.on("prefs", function (prefs) {
 	document.getElementById("lineSeparator").checked = parsedPrefs.lineSeparator;
 	document.getElementById("currentURL").checked = parsedPrefs.currentURL;
 	document.getElementById("saveMode").value = parsedPrefs.saveMode;
+	document.getElementById("confirmPanel").checked = parsedPrefs.confirmPanel;
 });
