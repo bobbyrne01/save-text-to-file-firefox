0f0bd9c68f845ec551363fe126004a931264002d
Robert Byrne <bobbyrne01@gmail.com>
2018-11-15 20:11:13 +0000

    fix copyright from copy/paste

--- backgroundScript.js
+++ backgroundScript.js
@@ -1,7 +1,5 @@
 'use strict';
 /*******************************************************************************
- * Copyright IBM Corp. 2017
-
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
